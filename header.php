96a98fbe23f91b2e5a0ccb581bf273f25865b0ce
nutjane <nutjane131@gmail.com>
2015-07-20 18:03:18 +0700

    code improvement
    - minor bug fixes

--- header.php
+++ header.php
@@ -16,19 +16,19 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?php  echo get_bloginfo('description'); ?>">
+    <!--<meta name="description" content="<?php  echo get_bloginfo('description'); ?>">-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php  wp_title( '|', true, 'right' ); ?></title>
     <!-- Add to homescreen for Chrome on Android -->
     <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="192x192" href="<?php  echo esc_url( get_template_directory_uri() ); ?>/images/touch/chrome-touch-icon-192x192.png">
+    <!--<link rel="icon" sizes="192x192" href="<?php  echo esc_url( get_template_directory_uri() ); ?>/images/touch/chrome-touch-icon-192x192.png">-->
     <!-- Add to homescreen for Safari on iOS -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="<?php  wp_title( '|', true, 'right' ); ?>">
-    <link rel="apple-touch-icon-precomposed" href="<?php  echo esc_url( get_template_directory_uri() ); ?>/apple-touch-icon-precomposed.png">
+    <!--<link rel="apple-touch-icon-precomposed" href="<?php  echo esc_url( get_template_directory_uri() ); ?>/apple-touch-icon-precomposed.png">-->
     <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="<?php  echo esc_url( get_template_directory_uri() ); ?>/images/touch/ms-touch-icon-144x144-precomposed.png">
+    <!--<meta name="msapplication-TileImage" content="<?php  echo esc_url( get_template_directory_uri() ); ?>/images/touch/ms-touch-icon-144x144-precomposed.png">-->
     <meta name="msapplication-TileColor" content="#3372DF">
     <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
     <!--
